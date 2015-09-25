a5b556da640d924b61c34c988fbd552887683484
Alex Leblang <alex.leblang@cloudera.com>
2015-09-25 11:32:53 -0700

    Adding hadoop conf for VM

--- vm_env.sh
+++ vm_env.sh
@@ -2,3 +2,4 @@ export RECORD_SERVICE_QUICKSTART_VM=True
 export MAVEN_OPTS="-Xmx2g -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=512m "
 export RECORD_SERVICE_PLANNER_HOST=quickstart.cloudera
 export HADOOP_CONF_DIR=`pwd`/conf/
+
